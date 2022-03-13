10800349e9937ff899bfc43037776d58655b64a2
nt7755 <nt7755@arcsoft.com.cn>
2022-03-13 18:29:06 +0800

    [InOneWeekend] anti-aliasing

--- src/InOneWeekend/main.cpp
+++ src/InOneWeekend/main.cpp
@@ -1,4 +1,5 @@
 #include "color.h"
+#include "camera.h"
 #include "rtweekend.h"
 #include "hittable_list.h"
 #include "sphere.h"
@@ -22,6 +23,7 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
 
     // World
     hittable_list world;
@@ -29,16 +31,7 @@ int main() {
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0));
 
     // Camera
-
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto facal_length = 1.0; // distance between the projection plane and the projection point
-
-    auto origin = point3(0, 0, 0); // position of camera center or "eye"
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, facal_length);
-
+    camera cam;
 
     // Render
 
@@ -50,12 +43,15 @@ int main() {
         for (int i = 0; i < image_width; ++i)
         {
             // caculate the color:
+            color pixel_color(0.0, 0.0, 0.0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world);
+            }
 
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
--- src/common/rtweekend.h
+++ src/common/rtweekend.h
@@ -2,6 +2,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -20,6 +21,23 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+// Returns a random real in [0, 1).
+inline double random_double() {
+	return rand() / (RAND_MAX + 1.0);
+}
+
+// Returns a random real in [min, max).
+inline double random_double(double min, double max) {
+	return min + (max - min) * random_double();
+}
+
+// clamp the value x to the range [min, max].
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
 }
 
 // Common Headers
